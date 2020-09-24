c65db6beaaa22d32cbaa7a24d07e1777e7dfb2eb
RaKXeR <afonsodelcb@gmail.com>
2020-09-24 22:08:58 +0100

    updated for website changes + intellij
    - The place where the data was stored and the way it was stored changed slightly, so the code had to change a bit;
    - Moved from Netbeans to Intellij;

--- src/main/java/rakxer/jbandcampscraper/Song.java
+++ src/main/java/rakxer/jbandcampscraper/Song.java
@@ -67,12 +67,13 @@ public class Song {
         }
         
         String html = IOUtils.toString(get.getResponseBodyAsStream(), "utf-8");
-        html = html.split("var SiteData", 2)[1].split("CurrencyData", 2)[0]; //reduces the size of the html string by 90+% to speed up future regex
-        
-        Matcher matcher = Pattern.compile("artist:\\s\"([^\"]*)").matcher(html);
+        html = html.split("data-tralbum=", 2)[1].split("data-cart=", 2)[0]; //reduces the size of the html string by 90+% to speed up future regex
+        html = html.replace("&quot;", "\"");
+
+        Matcher matcher = Pattern.compile("artist\":\\s*\"([^\"]*)").matcher(html);
         artist = matcher.find() ? matcher.group(1) : null; //this artist variable is not the correct artist variable, but values are the same, so it should be fine
         System.out.println(artist);
-        matcher = Pattern.compile("art_id\"*:\\s([^,]*)").matcher(html);
+        matcher = Pattern.compile("\"art_id\":\\s*([^,]*)").matcher(html);
         artURL = matcher.find() ? "https://f4.bcbits.com/img/a" + matcher.group(1) + "_16.jpg" : null; //same deal as with the artist variable
         System.out.println(artURL);
         
@@ -80,8 +81,8 @@ public class Song {
             throw new RuntimeException("Couldn't find all the necessary variables on the webpage provided (bandcamp)." + artist + artURL);
         }
         matcher = Pattern.compile("title\":\"([^\"]*)").matcher(html);
-        //must discard the first result because that's the "current" track, which doesn't have all info and the info that it does have is duped
-        matcher.find();
+        //discard last result
+        int find_count = 0;
         while (matcher.find()) {
             String title;
             //tries to check if the track name already includes the artist's name. if it doesn't, it adds it.
@@ -91,8 +92,12 @@ public class Song {
                 title = artist + " - " + matcher.group(1);
             }
             songs.add(new Song(title, null, artURL, 0));
+            find_count++;
+            if (find_count == matcher.groupCount()) {
+                break;
+            }
         }
-        matcher = Pattern.compile("128\":\"[^/]*/([^\"]*)").matcher(html);
+        matcher = Pattern.compile("128\":\"[^/]*/*([^\"]*)").matcher(html);
         for (int i = 0; i < songs.size() && matcher.find(); i++) {
             songs.get(i).setStreamingURL(matcher.group(1));
         }
