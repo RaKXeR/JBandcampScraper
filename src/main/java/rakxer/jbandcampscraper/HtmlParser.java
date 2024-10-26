39eefe39a62bb2148a7f6b73883938bc2c4d5172
RaKXeR <2105412+RaKXeR@users.noreply.github.com>
2024-10-26 02:50:31 +0100

    Replaced all HTML libraries with Java 11 HTTP

--- src/main/java/rakxer/jbandcampscraper/HtmlParser.java
+++ src/main/java/rakxer/jbandcampscraper/HtmlParser.java
@@ -1,33 +1,34 @@
 package rakxer.jbandcampscraper;
 
-import org.apache.hc.client5.http.classic.methods.HttpGet;
-import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
-import org.apache.hc.client5.http.impl.classic.CloseableHttpResponse;
-import org.apache.hc.client5.http.impl.classic.HttpClients;
-import org.apache.hc.core5.http.ParseException;
-import org.apache.hc.core5.http.io.entity.EntityUtils;
-
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 public class HtmlParser {
 
-    public String getPage(String url) {
-        String html;
-        CloseableHttpClient client = HttpClients.createDefault();
-        HttpGet get = new HttpGet(url);
+    private final HttpClient client;
 
-        try (CloseableHttpResponse response = client.execute(get)) {
-            int errcode = response.getCode();
-            if (errcode != 200) {
-                throw new RuntimeException("Http error " + errcode);
+    public HtmlParser() {
+        this.client = HttpClient.newHttpClient();
+    }
+
+    public String getPage(String url) {
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .GET()
+                .build();
+
+        try {
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            if (response.statusCode() != 200) {
+                throw new RuntimeException("Http error " + response.statusCode());
             }
-            html = EntityUtils.toString(response.getEntity(), StandardCharsets.UTF_8);
-        } catch (IOException | ParseException e) {
+            return response.body();
+        } catch (IOException | InterruptedException e) {
             throw new RuntimeException("Couldn't get webpage", e);
         }
-
-        return html;
     }
 
 }
