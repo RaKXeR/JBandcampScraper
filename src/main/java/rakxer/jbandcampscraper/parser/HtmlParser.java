e23b5b6aa8b48c55e370ae9f3ad24a7e74dca3bc
RaKXeR <2105412+RaKXeR@users.noreply.github.com>
2024-10-26 17:09:54 +0100

    Added new Jsoup HTML parser

---
+++ src/main/java/rakxer/jbandcampscraper/parser/HtmlParser.java
@@ -0,0 +1,52 @@
+package rakxer.jbandcampscraper.parser;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.safety.Safelist;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class HtmlParser {
+
+    private final HttpClient client;
+
+    public HtmlParser() {
+        this.client = HttpClient.newHttpClient();
+    }
+
+    public String getPage(String url) {
+        String html = getHtml(url);
+        Safelist safelist = Safelist.relaxed()
+                .addTags("script")
+                .addAttributes("script", "src", "data-tralbum")
+                ;
+
+        // Clean and format the HTML
+        Document cleanDocument = Jsoup.parse(Jsoup.clean(html, safelist));
+        cleanDocument.outputSettings().prettyPrint(true).indentAmount(4);
+
+        return cleanDocument.toString();
+    }
+
+    private String getHtml(String url) {
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .GET()
+                .build();
+
+        try {
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            if (response.statusCode() != 200) {
+                throw new RuntimeException("Http error " + response.statusCode());
+            }
+            return response.body();
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException("Couldn't get webpage", e);
+        }
+    }
+
+}
