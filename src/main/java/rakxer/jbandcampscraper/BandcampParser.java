f6b361371b71aa44d9b774fec565f478855165b2
RaKXeR <2105412+RaKXeR@users.noreply.github.com>
2024-10-26 01:14:58 +0100

    Cleaned up BandcampParser
    Separated monolithic method into proper responsibilities

--- src/main/java/rakxer/jbandcampscraper/BandcampParser.java
+++ src/main/java/rakxer/jbandcampscraper/BandcampParser.java
@@ -10,58 +10,102 @@ import org.apache.hc.core5.http.io.entity.EntityUtils;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class BandcampParser {
 
-    public ArrayList<Song> getSongs(String url) throws IOException {
-        String html = getPageHtml(url);
-        ArrayList<Song> songs = new ArrayList<>();
-        String artist, artURL;
+    private final List<Song> songs = new ArrayList<>();
+    private String artist;
 
-        html = html.split("data-tralbum=", 2)[1].split("data-cart=", 2)[0]; //reduces the size of the html string by 90+% to speed up future regex
-        html = html.replace("&quot;", "\"");
+    public List<Song> getSongs(String url) {
+        String html = getPage(url);
+        String artURL;
 
-        Matcher matcher = Pattern.compile("artist\":\\s*\"([^\"]*)").matcher(html);
-        artist = matcher.find() ? matcher.group(1) : null; //this artist variable is not the correct artist variable, but values are the same, so it should be fine
+        html = prune(html);
+
+        artist = getArtist(html);
         System.out.println(artist);
-        matcher = Pattern.compile("\"art_id\":\\s*([^,]*)").matcher(html);
-        artURL = matcher.find() ? "https://f4.bcbits.com/img/a" + matcher.group(1) + "_16.jpg" : null; //same deal as with the artist variable
+        artURL = getArtURL(html);
         System.out.println(artURL);
 
         if (artist == null || artURL == null) {
             throw new RuntimeException("Couldn't find all the necessary variables on the webpage provided (bandcamp)." + artist + artURL);
         }
-        matcher = Pattern.compile("title\":\"([^\"]*)").matcher(html);
-        //discard last result
-        int find_count = 0;
-        while (matcher.find()) {
+
+        List<String> titles = getSongTitles(html);
+
+        for (String title : titles) {
+            songs.add(new Song(title, null, artURL, 0));
+        }
+
+        fillStreamingURLs(html);
+        fillSongDurations(html);
+
+        return songs;
+    }
+
+    private void fillSongDurations(String html) {
+        Matcher matcher = Pattern.compile("duration\":([^,]*)").matcher(html);
+        for (int i = 0; i < songs.size() && matcher.find(); i++) {
+            songs.get(i).setDuration(Double.parseDouble(matcher.group(1)));
+        }
+    }
+
+    private void fillStreamingURLs(String html) {
+        Matcher matcher = Pattern.compile("128\":\"[^/]*/*([^\"]*)").matcher(html);
+        for (int i = 0; i < songs.size() && matcher.find(); i++) {
+            songs.get(i).setStreamingURL(matcher.group(1));
+        }
+    }
+
+    private List<String> getSongTitles(String html) {
+        List<String> titles = new ArrayList<>();
+        Matcher matcher = Pattern.compile("title\":\"([^\"]*)").matcher(html);
+
+        for (int find_count = 0; matcher.find(); find_count++) {
             String title;
-            //tries to check if the track name already includes the artist's name. if it doesn't, it adds it.
+            // Check if the track name already includes the artist's name. If it doesn't, add it.
             if (matcher.group(1).contains(" - ")) {
                 title = matcher.group(1);
             } else {
                 title = artist + " - " + matcher.group(1);
             }
-            songs.add(new Song(title, null, artURL, 0));
-            find_count++;
-            if (find_count == matcher.groupCount()) {
+            titles.add(title);
+
+            if (find_count + 1 == matcher.groupCount()) {
                 break;
             }
         }
-        matcher = Pattern.compile("128\":\"[^/]*/*([^\"]*)").matcher(html);
-        for (int i = 0; i < songs.size() && matcher.find(); i++) {
-            songs.get(i).setStreamingURL(matcher.group(1));
-        }
-        matcher = Pattern.compile("duration\":([^,]*)").matcher(html);
-        for (int i = 0; i < songs.size() && matcher.find(); i++) {
-            songs.get(i).setDuration(Double.parseDouble(matcher.group(1)));
-        }
-        return songs;
+
+        return titles;
     }
 
-    private String getPageHtml(String url) {
+    private String getArtURL(String html) {
+        String artURL;
+        Matcher matcher = Pattern.compile("\"art_id\":\\s*([^,]*)").matcher(html);
+        // Not the correct art URL, but values are the same, so it should be fine
+        artURL = matcher.find() ? "https://f4.bcbits.com/img/a" + matcher.group(1) + "_16.jpg" : null;
+        return artURL;
+    }
+
+    private String getArtist(String html) {
+        String artist;
+        Matcher matcher = Pattern.compile("artist\":\\s*\"([^\"]*)").matcher(html);
+        // Not the correct artist variable, but values are the same, so it should be fine
+        artist = matcher.find() ? matcher.group(1) : null;
+        return artist;
+    }
+
+    private String prune(String html) {
+        // Reduces the size of the html string by 90+% to speed up future regex
+        html = html.split("data-tralbum=", 2)[1].split("data-cart=", 2)[0];
+        html = html.replace("&quot;", "\"");
+        return html;
+    }
+
+    private String getPage(String url) {
         String html;
         CloseableHttpClient client = HttpClients.createDefault();
         HttpGet get = new HttpGet(url);
@@ -74,7 +118,7 @@ public class BandcampParser {
 
             html = EntityUtils.toString(response.getEntity(), StandardCharsets.UTF_8);
         } catch (IOException | ParseException e) {
-            throw new RuntimeException("Couldn't get the webpage from Bandcamp");
+            throw new RuntimeException("Couldn't get webpage", e);
         }
 
         return html;
