b4ed0f70a7f2b92c056fbaaaaa8fb81addadde44
RaKXeR <2105412+RaKXeR@users.noreply.github.com>
2024-10-26 17:12:25 +0100

    Added Builder for Song data

--- src/main/java/rakxer/jbandcampscraper/Song.java
+++ src/main/java/rakxer/jbandcampscraper/Song.java
@@ -1,22 +1,69 @@
 package rakxer.jbandcampscraper;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor // TODO: Remove once old parsers are removed
 public class Song {
     private String title, streamingURL, artURL;
     private double duration;
 
-    public Song(String title, String streamingURL, String artURL, double duration) {
-        this.title = title;
-        this.streamingURL = streamingURL;
-        this.artURL = artURL;
-        this.duration = duration;
+    private Song(Builder builder) {
+        this.title = builder.title;
+        this.streamingURL = builder.streamingURL;
+        this.artURL = builder.artURL;
+        this.duration = builder.duration;
     }
 
-    @Override
-    public String toString() {
-        return String.format("{%s, %s, %s, %f}", title, streamingURL, artURL, duration);
+    public static class Builder {
+        private String artist, title, streamingURL, artId, artURL;
+        private Double duration;
+
+        public Builder artist(String artist) {
+            this.artist = artist;
+            return this;
+        }
+
+        public Builder title(String title) {
+            this.title = title;
+            return this;
+        }
+
+        public Builder streamingURL(String streamingURL) {
+            this.streamingURL = streamingURL;
+            return this;
+        }
+
+        public Builder artId(String artId) {
+            this.artId = artId;
+            return this;
+        }
+
+        public Builder duration(double duration) {
+            this.duration = duration;
+            return this;
+        }
+
+        private String getFormattedTitle() {
+            boolean hasArtistInTitle = title.contains(" - ");
+            return hasArtistInTitle ? title : artist + " - " + title;
+        }
+
+        private String getArtURL() {
+            return String.format("https://f4.bcbits.com/img/a%s_16.jpg", artId);
+        }
+
+        public Song build() {
+            if (title == null || streamingURL == null || duration == null) {
+                throw new IllegalStateException("Title, streamingURL and duration must be set");
+            }
+
+            title = getFormattedTitle();
+            artURL = getArtURL();
+            return new Song(this);
+        }
+
     }
 
 }
