ac67ecea0c47506ac07d89296ab21be715fed207
RaKXeR <2105412+RaKXeR@users.noreply.github.com>
2024-10-26 16:46:50 +0100

    Simplified file reading in test package

--- src/test/java/BandcampParserStub.java
+++ src/test/java/BandcampParserStub.java
@@ -2,7 +2,7 @@ import rakxer.jbandcampscraper.parser.old.BandcampParser;
 
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.nio.file.Path;
 
 import static org.junit.jupiter.api.Assertions.fail;
 
@@ -18,8 +18,7 @@ public class BandcampParserStub extends BandcampParser {
     @Override
     protected String getPage(String ignored) {
         try {
-            // Read the contents of the file as a single String
-            return new String(Files.readAllBytes(Paths.get(FILE_PATH)));
+            return Files.readString(Path.of(FILE_PATH));
         } catch (IOException e) {
             fail("Couldn't read the file: " + FILE_PATH);
             throw new RuntimeException("Unreachable code");
