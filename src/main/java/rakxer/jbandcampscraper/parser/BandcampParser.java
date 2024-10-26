f3ba0a4ac065a7d65d973f9f8af1447197868e23
RaKXeR <2105412+RaKXeR@users.noreply.github.com>
2024-10-26 17:13:31 +0100

    Wrote parser using new logic

---
+++ src/main/java/rakxer/jbandcampscraper/parser/BandcampParser.java
@@ -0,0 +1,38 @@
+package rakxer.jbandcampscraper.parser;
+
+import rakxer.jbandcampscraper.Song;
+import rakxer.jbandcampscraper.dto.Album;
+import rakxer.jbandcampscraper.mapper.JsonAlbumMapper;
+
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class BandcampParser {
+
+    private String html;
+
+    public void setURL(String url) {
+        if (!isValidURL(url)) {
+            throw new IllegalArgumentException("Invalid bandcamp URL");
+        }
+        html = getPage(url);
+    }
+
+    public List<Song> getSongs() {
+        AlbumParser albumParser = new AlbumParser();
+        Album album = albumParser.getAlbum(html);
+        return JsonAlbumMapper.getSongs(album);
+    }
+
+    public static boolean isValidURL(String url) {
+        Matcher matcher = Pattern.compile("\\w*\\.bandcamp.com/(track|album)/[^/]*/?$").matcher(url);
+        return matcher.find();
+    }
+
+    protected String getPage(String url) {
+        HtmlParser parser = new HtmlParser();
+        return parser.getPage(url);
+    }
+
+}
