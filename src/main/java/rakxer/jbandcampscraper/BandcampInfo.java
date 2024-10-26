269e322265d6f3fd59c9758d914197b63ac37072
RaKXeR <2105412+RaKXeR@users.noreply.github.com>
2024-10-26 01:22:53 +0100

    Cleaned up main method

--- src/main/java/rakxer/jbandcampscraper/BandcampInfo.java
+++ src/main/java/rakxer/jbandcampscraper/BandcampInfo.java
@@ -1,12 +1,10 @@
 package rakxer.jbandcampscraper;
 
-import java.io.IOException;
 import java.util.List;
 import java.util.Scanner;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 
 public class BandcampInfo {
@@ -16,18 +14,13 @@ public class BandcampInfo {
         System.out.println("Insert a bandcamp URL: ");
         String url = s.next().trim();
 
-        if (isValidBandcampURL(url)) {
-            try {
-                List<Song> songs = getSongs(url);
-                songs.stream().forEach(song -> {
-                    System.out.println(song.toString());
-                });
-            } catch (IOException ex) {
-                Logger.getLogger(BandcampInfo.class.getName()).log(Level.SEVERE, null, ex);
-            }
-        } else {
+        if (!isValidBandcampURL(url)) {
             System.out.println("The URL you inserted wasn't a valid bandcamp link.");
+            throw new IllegalArgumentException("Invalid URL");
         }
+
+        List<String> songs = getSongs(url);
+        songs.forEach(System.out::println);
     }
 
     private static boolean isValidBandcampURL(String url) {
@@ -35,8 +28,9 @@ public class BandcampInfo {
         return matcher.find();
     }
 
-    private static List<Song> getSongs(String url) throws IOException {
-        return new BandcampParser().getSongs(url);
+    private static List<String> getSongs(String url) {
+        List<Song> songs = new BandcampParser().getSongs(url);
+        return songs.stream().map(Song::toString).collect(Collectors.toList());
     }
 
 }
